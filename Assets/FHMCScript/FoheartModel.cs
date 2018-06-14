c540cd6f19a8e99356a93f52eae568e994741e25
FOHEART <humaoxiao@foheart.com>
2018-06-14 09:01:55 +0800

    merge from v1.3.6

---
+++ Assets/FHMCScript/FoheartModel.cs
@@ -0,0 +1,393 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System;
+
+namespace FoheartMC
+{
+    public class FoheartModel : MonoBehaviour
+    {
+        /**
+         * ConnectPackNumber
+         * 本模型连接的设备包号.0为没有连接
+         * 则该值会在设备连接是自动指定,但可能不是你想要的那个模型,此时需要手动指定包号
+         */
+        public string ActorName;
+        //接受哪个模型发来的数据
+        public string ConfigName;
+
+        //是否使用远程位移
+        public Vector3 HipsStartLocation;
+		public bool FixHipsLocation;
+
+		public String[] BonesName;
+		public Vector3[] BonesLocation=new Vector3[23];
+		public String[] SensorName;
+		public Vector3[] SensorAccel=new Vector3[23];
+		public Vector3[] SensorGyro=new Vector3[23];
+		public Vector3[] SensorMag=new Vector3[23];
+
+        string OutText;
+        //调试输出的文本,可以在任意时候更改,
+        [HideInInspector]
+        public uint ConnectPackNumber;
+        //链接的包号
+        protected Dictionary<uint, string> mapBoneIndex;
+        class initAxisAndRot
+        {
+            public int[] xyz;
+
+            public float[] xryrzr;
+
+            public initAxisAndRot()
+            {
+                xyz = new int[3];
+                xryrzr = new float[3];
+            }
+            public void setAxis(int _x, int _y, int _z)
+            {
+                xyz[0] = _x; xyz[1] = _y; xyz[2] = _z;
+            }
+            public void setRot(float _xr, float _yr, float _zr)
+            {
+                xryrzr[0] = _xr; xryrzr[1] = _yr; xryrzr[2] = _zr;
+            }
+        }
+        Dictionary<uint, initAxisAndRot> mapBoneInitAxisTrans;
+        protected bool dataPending = false;
+        protected Dictionary<string, Transform> m_mapNameAndBone;
+        protected Quaternion m_bodyStartQuat;
+        protected Vector3 m_bodyStartPosition;
+        ActorFrameData frameDataBuffer;
+
+        public FoheartModel()
+        {
+			
+			BonesName = new string[23]{ 
+				"Pelvis", 
+				"L5" , 
+				"L3", 
+				"T12",
+				"T8",
+				"Neck", 
+				"Head",
+				"RightShoulder", 
+				"RightUpperArm", 
+				"RightForeArm", 
+				"RightHand", 
+				"LeftShoulder",
+				"LeftUpperArm", 
+				"LeftForeArm" , 
+				"LeftHand",
+				"RightUpperLeg",
+				"RightLowerLeg",
+				"RightFoot", 
+				"RightToe", 
+				"LeftUpperLeg", 
+				"LeftLowerLeg", 
+				"LeftFoot", 
+				"LeftToe"
+			};
+
+			SensorName = new String[23]{
+				"Hip",
+				"Not Attach Sensor",
+				"Not Attach Sensor",
+				"Chest",
+				"Not Attach Sensor",
+				"Not Attach Sensor",
+				"Head",
+				"Left Shoulder",
+				"Left Upper Arm",
+				"Left Lower Arm",
+				"Left Hand",
+				"Right Shoulder",
+				"Right Upper Arm",
+				"Right Lower Arm",
+				"Right Hand",
+				"Left Thigh",
+				"Left Shank",
+				"Left Foot",
+				"Not Attach Sensor",
+				"Right Thigh",
+				"Right Shank",
+				"Right Foot",
+				"Not Attach Sensor",
+			};
+
+			ActorName = "Actor1(Live)";
+            ConfigName = "DefaultActor.xml";
+        }
+
+        void Awake()
+        {
+            CheckCondition();
+            mapBoneIndex = new Dictionary<uint, string>();
+            m_mapNameAndBone = new Dictionary<string, Transform>();
+            mapBoneInitAxisTrans = new Dictionary<uint, initAxisAndRot>();
+            frameDataBuffer = new ActorFrameData();
+            loadBoneMaps();
+            initNameAndBone();
+            ChildAwake();
+
+            Transform HipsStartTrans = FindBone(0);
+            if (HipsStartTrans == null)
+            {
+                Debug.LogError("Please check Config Name is Right or ConnectId's name is valid in Model!");
+            }
+            HipsStartLocation = HipsStartLocation;// +new Vector3(HipsStartTrans.position.x, HipsStartTrans.position.y, HipsStartTrans.position.z);
+        }
+
+
+        //检查是否已经指定骨骼绑定文件
+        void CheckCondition()
+        {
+            if (ConfigName.Length == 0)
+            {
+                //没有指定骨骼配置文件,请先编辑好配置文件,放置在工程目录下(Assets的父目录)
+                //并手动将文件名,填写到ConfigName中
+                Debug.LogError("ConfigName not specified!");
+            }
+            if (ActorName.Length == 0)
+            {
+                //该模型接受的动作角色名称
+                Debug.LogError("ActorReceiveName not specified!");
+            }
+        }
+
+        //继承自本类的组件,初始化,需要重写ChildAwake,来达到初始化效果,虚函数
+        public virtual void ChildAwake()
+        {
+        }
+
+        //准备开始
+        void Start()
+        {
+            m_bodyStartQuat = transform.rotation;
+        }
+
+        //UI界面
+        void OnGUI()
+        {
+            //调试输出的内容
+            GUI.Label(new Rect(0, 10, 500, 300), OutText);
+        }
+
+        //从mapBoneIndex中初始化
+        void initNameAndBone()
+        {
+            List<Transform> list = new List<Transform>();
+            list.AddRange(gameObject.GetComponentsInChildren<Transform>());
+            foreach (var deFineBone in mapBoneIndex)
+            {
+                bool bFind = false;
+                foreach (Transform BoneObj in list)
+                {
+                    if (BoneObj.name == deFineBone.Value)
+                    {
+                        m_mapNameAndBone.Add(BoneObj.name, BoneObj);
+                        bFind = true;
+                        break;
+                    }
+                }
+                if (!bFind)
+                {
+                    print(deFineBone.Value + " not found in the model!");
+                }
+            }
+        }
+
+        //从配置文件加载骨骼映射
+        void loadBoneMaps()
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(ConfigName);
+                foreach (XElement element in doc.Element("ActorBones").Elements())
+                {
+                    uint BoneID = Convert.ToUInt32(element.Attribute("ConnectId").Value);
+                    mapBoneIndex.Add(BoneID, element.Attribute("name").Value);
+
+                    if (mapBoneInitAxisTrans.Count == 0 || !mapBoneInitAxisTrans.ContainsKey(BoneID))
+                        mapBoneInitAxisTrans.Add(BoneID, new initAxisAndRot());
+
+                    /*--------------Get local axis conversion-----------------*/
+                    string[] expectAxisAttr = new string[3] { "X", "Y", "Z" };
+                    string[] standardAxisValue = new string[6] { "+X", "-X", "+Y", "-Y", "+Z", "-Z" };
+                    int[] initAxis = new int[3] { 0, 0, 0 };
+                    for (int i = 0; i < expectAxisAttr.Length; i++)
+                    {
+                        string convertedAttr = "";
+                        if (element.Attribute(expectAxisAttr[i]) == null || element.Attribute(expectAxisAttr[i]).Value == "")
+                        {
+                            convertedAttr = expectAxisAttr[i];
+                        }
+                        else
+                        {
+                            convertedAttr = element.Attribute(expectAxisAttr[i]).Value.ToUpper();
+                        }
+
+                        if (convertedAttr.Length == 1)
+                        {
+                            convertedAttr = "+" + convertedAttr;
+                        }
+                        int[] singleInitAxis = new int[3];
+                        for (int j = 0; j < standardAxisValue.Length; j++)
+                        {
+                            if (convertedAttr == standardAxisValue[j])
+                            {
+                                singleInitAxis[i] = (j + 1) % 2 == 0 ? -((j + 2) / 2) : ((j + 2) / 2);
+                                break;
+                            }
+                        }
+
+                        for (int k = 0; k < 3; k++)
+                        {
+                            initAxis[k] += singleInitAxis[k];
+                        }
+                    }
+                    mapBoneInitAxisTrans[BoneID].setAxis(
+                        initAxis[0],
+                        initAxis[1],
+                        initAxis[2]
+                        );
+                    /*--------------Get initial local rotation-----------------*/
+                    float[] initRot = new float[3];
+                    string[] expectRotAttr = new string[3] { "XR", "YR", "ZR" };
+
+                    for (int i = 0; i < expectRotAttr.Length; i++)
+                    {
+                        if (element.Attribute(expectRotAttr[i]) == null || element.Attribute(expectRotAttr[i]).Value == "")
+                        {
+                            initRot[i] = 0.0f;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                initRot[i] = Convert.ToSingle(element.Attribute(expectRotAttr[i]).Value);
+                            }
+                            catch (Exception e)
+                            {
+                                print("[ERROR] " + e.ToString() + " Failed conversion, please check your input!");
+                                initRot[i] = 0.0f;
+                            }
+                        }
+                    }
+                    mapBoneInitAxisTrans[BoneID].setRot(
+                        initRot[0],
+                        initRot[1],
+                        initRot[2]
+                        );
+
+                }
+            }
+            catch (Exception e)
+            {
+                OutText = "@num" + e.ToString();
+            }
+        }
+
+        //this function called by The UDP thread
+        public void copyData(ActorFrameData data)
+        {
+            lock (frameDataBuffer)
+            {
+                data.CopyTo(ref frameDataBuffer);
+                dataPending = true;
+            }
+        }
+
+        // Update is called once per frame after Update
+        void Update()
+        {
+            lock (frameDataBuffer)
+            {
+                if (!dataPending)
+                {
+                    return;
+                }
+                applyBoneTransRot(frameDataBuffer);
+            }
+        }
+
+        public virtual void applyBoneTransRot(ActorFrameData data)
+        {
+            Transform BoneHip = FindBone(0);
+            if (BoneHip)
+            {
+                Vector3 LocInUnity = new Vector3();
+                UInt32 scale = 1;
+                LocInUnity.Set(
+                    (-data.bonePositions[0].x + HipsStartLocation.x) * scale,
+                    (data.bonePositions[0].z+HipsStartLocation.y) * scale,
+                    (-data.bonePositions[0].y+HipsStartLocation.z) * scale
+				              );
+				for (int i = 0; i < 23; i++) {
+					Byte ind = (Byte)i;
+					BonesLocation [i].Set (data.bonePositions[ind].x,data.bonePositions[ind].x,data.bonePositions[ind].x);
+					//SensorAccel [i].Set (data.sensorAccels [ind].x, data.sensorAccels [ind].y, data.sensorAccels [ind].z);
+				}
+				for (int i = 0; i < 23; i++) {
+					Byte ind = (Byte)i;
+					//BonesLocation [i].Set (data.bonePositions[ind].x,data.bonePositions[ind].x,data.bonePositions[ind].x);
+					SensorAccel [i].Set (data.sensorAccels [ind].x, data.sensorAccels [ind].y, data.sensorAccels [ind].z);
+				}
+                if (FixHipsLocation)
+                {
+                }
+                else
+                {
+                    BoneHip.transform.position = LocInUnity;
+                }
+            }
+            else
+            {
+                print("[ERROR] Do not find HIP, can not set actor location.");
+                return;
+            }
+
+            foreach (var BoneR in data.boneRotQuat)
+            {
+                Transform BoneT = FindBone(BoneR.Key);
+                if (BoneT)
+                {
+                    var BR = BoneR.Value;
+
+                    float[] remoteRot = new float[3] { BR.x, BR.y, BR.z };
+                    float[] transRot = new float[3];
+                    for (int i = 0; i < transRot.Length; i++)
+                    {
+                        bool isNeg;
+                        if (mapBoneInitAxisTrans[BoneR.Key].xyz[i] < 0) isNeg = true; else isNeg = false;
+                        int tempIndex = isNeg ? (-mapBoneInitAxisTrans[BoneR.Key].xyz[i]) - 1 : mapBoneInitAxisTrans[BoneR.Key].xyz[i] - 1;
+                        transRot[i] = remoteRot[tempIndex] * (isNeg ? -1 : 1);
+                    }
+
+                    Quaternion convQuatApply = new Quaternion(transRot[0], transRot[1], transRot[2], BR.w);
+                    Quaternion rot = Quaternion.Euler(
+                        mapBoneInitAxisTrans[BoneR.Key].xryrzr[0],
+                        mapBoneInitAxisTrans[BoneR.Key].xryrzr[1],
+                        mapBoneInitAxisTrans[BoneR.Key].xryrzr[2]);
+                    BoneT.localRotation = rot * convQuatApply;
+                }
+            }
+        }
+
+        //快捷查找骨骼节点.如果没有找到则返回 null
+        protected Transform FindBone(uint BoneIndex)
+        {
+            string boneName;
+            if (mapBoneIndex.TryGetValue(BoneIndex, out boneName) == false)
+            {
+                return null;
+            }
+            Transform transform;
+            m_mapNameAndBone.TryGetValue(boneName, out transform);
+            return transform;
+        }
+    }
+}
---
+++ Assets/FHMCScript/NetFrameDataReceiver.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+namespace FoheartMC
+{
+    public class NetFrameDataReceiver : MonoBehaviour
+    {
+        //UDP命令下发IP地址
+        public string UDP_TargetIP = "127.0.0.1";
+        public int UDP_Target_Port = 5002;
+        //UDP广播接收端口
+        public int UDPPort;
+        //校准类型定义
+        private int CalibrateType_standard = 0x0FFFFFF1;
+        private int CalibrateType_arm = 0x0FFFFFF2;
+        private int CalibrateType_standard_arm = 0x0FFFFFF3;
+        //角色列表,需要手动指定
+        public FoheartModel[] PlayerList;
+        public bool BonePosition;
+        public bool BoneEuler;
+        public bool BoneQuat;
+		public bool SensorAccel;
+        //调试文本
+        string OutText;
+
+        public UI_Calibrate Calibrate;
+        
+        public NetFrameDataReceiver()
+        {
+            UDPPort = 5001;
+            BonePosition = true;
+            BoneEuler = true;
+            BoneQuat = false;
+			SensorAccel = false;
+            udpSender = new System.Net.Sockets.UdpClient();
+            
+        }
+
+        //初始化
+        void Start()
+        {
+            if (true == BoneEuler && true == BoneQuat)
+            {
+                Debug.Log("You can not set BoneEuler and BoneQuat at the same time.");
+                return;
+            }
+            initRec();
+        }
+
+        //UI界面
+        void OnGUI()
+        {
+            GUI.Label(new Rect(0, 0, 100, 50), OutText);
+
+            Calibrate.ShowCalibrateTips();
+
+        }
+        public void OnClickCalibration_standard()
+        {
+            //模型名称
+            string CurActorName = GameObject.Find("Canvas/ActorName").GetComponent<InputField>().text;
+
+            if (CurActorName == "")
+            {
+                Debug.Log("请输入校准模型的名称！");
+                return;
+            }
+
+            bool standard = GameObject.Find("Canvas/Toggle_standard").GetComponent<Toggle>().isOn;
+            bool arm = GameObject.Find("Canvas/Toggle_arm").GetComponent<Toggle>().isOn;
+
+            if(!standard && !arm)
+            {
+                Debug.Log("请选择校准方式！");
+                return;
+            }
+            int CurCalibrateType = CalibrateType_standard;
+            if (standard)
+                if (arm)
+                    CurCalibrateType = CalibrateType_standard_arm;
+                else
+                    CurCalibrateType = CalibrateType_standard;
+            else
+                if (arm)
+                    CurCalibrateType = CalibrateType_arm;
+                else
+                {
+                    Debug.Log("请选择校准方式！");
+                    return;
+                }
+
+            //下发校准命令
+            OnSendCalibrate(CurActorName, CurCalibrateType);
+            
+            Debug.Log("点击校准（标准）");
+        }
+        //UDP接收线程
+        Thread thrUDPRecv;
+        //UDP接收端
+        UdpClient udpReceiver;
+        //UDP发送端
+        private UdpClient udpSender;
+
+        //初始化接收器
+        void initRec()
+        {
+            udpReceiver = new UdpClient(new IPEndPoint(IPAddress.Any, UDPPort));
+            thrUDPRecv = new Thread(mcReceiveData);
+            thrUDPRecv.Start();
+
+            udpSender = new UdpClient(new IPEndPoint(IPAddress.Any, 0));
+            
+            Calibrate = new UI_Calibrate();
+        }
+
+        //接收数据的工作线程
+        void mcReceiveData()
+        {
+            //接收广播
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+            ActorFrameData frameDataTemp = new ActorFrameData();
+            while (true)
+            {
+                try
+                {
+                    byte[] data = udpReceiver.Receive(ref endpoint);
+
+                    if (data.Length == UI_Calibrate.STANDARD_UDP_STATUS_BUFFER_SIZE)
+                    {
+                        Calibrate.status = BitConverter.ToInt32(data, 0);
+                        Calibrate.second = BitConverter.ToInt32(data, Marshal.SizeOf(Calibrate.status));
+
+                    }else
+                    {
+						int dataErro = frameDataTemp.deComposeData(data, BonePosition, BoneEuler, BoneQuat,SensorAccel);
+                        if (dataErro != 0)
+                        {
+                            Debug.Log("Data Erro:" + dataErro);
+                            continue;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e.ToString());
+                }
+
+                foreach (FoheartModel model in PlayerList)
+                {
+                    if (string.Equals(model.ActorName, frameDataTemp.strActorName))
+                    {
+                        model.copyData(frameDataTemp);
+                    }
+                }
+            }
+        }
+        //发送命令
+        void OnSendCalibrate(string ActorName, int type)
+        {
+            try
+            {
+                Byte[] byteArrayData = System.Text.Encoding.Default.GetBytes(ActorName);
+                Int32 NameLen = byteArrayData.Length;
+                Byte[] sendBytes1 = BitConverter.GetBytes(NameLen);
+                Byte[] sendBytes2 = BitConverter.GetBytes(type);
+                /*  4 + nameLen + 4
+                    * 4字节：nameLen
+                    * nameLen：名字内容
+                    * 4字节：校准方式
+                    * */
+                Byte[] TotalData = new Byte[sendBytes1.Length + byteArrayData.Length + sendBytes2.Length];
+                System.Buffer.BlockCopy(sendBytes1, 0, TotalData, 0, sendBytes1.Length);
+                System.Buffer.BlockCopy(byteArrayData, 0, TotalData, sendBytes1.Length, byteArrayData.Length);
+                System.Buffer.BlockCopy(sendBytes2, 0, TotalData, sendBytes1.Length + byteArrayData.Length, sendBytes2.Length);
+
+                udpSender.Send(TotalData, TotalData.Length, UDP_TargetIP, UDP_Target_Port);
+
+            }
+            catch (Exception s)
+            {
+                Debug.Log(s.Message);
+            }
+
+        }
+
+        //脚本退出时,停止线程
+        void OnApplicationQuit()
+        {
+            thrUDPRecv.Abort();
+            Debug.Log("OnApplicationQuit...");
+        }
+
+        void OnDisable()
+        {
+            //结束接收
+            udpReceiver.Close();
+            udpSender = null;
+        }
+        //查找
+        FoheartModel getConnectModel(uint packNumber)
+        {
+            if (packNumber == 0)
+            {
+                return null;
+            }
+            //遍历已经连接的模型
+            foreach (FoheartModel player in PlayerList)
+            {
+                if (player && player.ConnectPackNumber == packNumber)
+                {
+                    return player;
+                }
+            }
+            //如果没有连接的模型,则指定一个,并保存下来
+            foreach (FoheartModel player in PlayerList)
+            {
+                if (player.ConnectPackNumber == 0)
+                {
+                    player.ConnectPackNumber = packNumber;
+                    return player;
+                }
+            }
+            //没有找到可用的模型,则返回空
+            return null;
+        }
+    }
+}
